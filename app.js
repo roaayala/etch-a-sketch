f53baba60cad3dbf978ad11133574c5877af82e3
roaayala <roaayala.ahm@gmail.com>
2024-09-22 13:22:34 +0800

    set width of container to 80vh

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const container = document.querySelector('.container');
 
-const gridSizes = prompt('How many pixel-grid do you want!');
+// const gridSizes = prompt('How many pixel-grid do you want!');
 
 const randomizeColor = () => Math.floor(Math.random() * 255 + 1);
 
-for (let i = 0; i < gridSizes ** 2; i++) {
-  const colorBoxes = document.createElement('div');
-  colorBoxes.style.backgroundColor = `rgb(${randomizeColor()}, ${randomizeColor()},${randomizeColor()})`;
-  colorBoxes.setAttribute('class', 'color-box');
-  colorBoxes.style.width = '16px';
-  colorBoxes.style.height = '16px';
-  container.append(colorBoxes);
-}
+// for (let i = 0; i < gridSizes ** 2; i++) {
+//   const colorBoxes = document.createElement('div');
+//   colorBoxes.style.backgroundColor = `rgb(${randomizeColor()}, ${randomizeColor()},${randomizeColor()})`;
+//   colorBoxes.setAttribute('class', 'color-box');
+//   colorBoxes.style.width = '16px';
+//   colorBoxes.style.height = '16px';
+//   container.append(colorBoxes);
+// }
 
-container.style.width = ` ${+gridSizes * 16}px`;
-container.style.height = ` ${+gridSizes * 16}px`;
+container.style.width = `100vw`;
+container.style.height = `100vh`;
 
 const colorBoxesAll = document.querySelectorAll('.color-box');
