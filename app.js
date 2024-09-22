e44b917c8e337a3cae8b556d978b968d87933b3f
roaayala <roaayala.ahm@gmail.com>
2024-09-22 11:13:44 +0800

    create color box from user input

---
+++ app.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('.container');
+
+const gridSizes = prompt('How many pixel-grid do you want!');
+
+const randomizeColor = () => Math.floor(Math.random() * 255 + 1);
+
+for (let i = 0; i < gridSizes ** 2; i++) {
+  const colorBoxes = document.createElement('div');
+  colorBoxes.style.backgroundColor = `rgb(${randomizeColor()}, ${randomizeColor()},${randomizeColor()})`;
+  colorBoxes.style.width = '16px';
+  colorBoxes.style.height = '16px';
+
+  container.append(colorBoxes);
+}
+
+container.style.width = ` ${+gridSizes * 16}px`;
+container.style.height = ` ${+gridSizes * 16}px`;
