51a9c572cacc1f5e305abb62f9a5a438bc9b56df
roaayala <roaayala.ahm@gmail.com>
2024-09-22 11:26:22 +0800

    working on how to change color box colors when hover event occurs

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const randomizeColor = () => Math.floor(Math.random() * 255 + 1);
 for (let i = 0; i < gridSizes ** 2; i++) {
   const colorBoxes = document.createElement('div');
   colorBoxes.style.backgroundColor = `rgb(${randomizeColor()}, ${randomizeColor()},${randomizeColor()})`;
+  colorBoxes.setAttribute('class', 'color-box');
   colorBoxes.style.width = '16px';
   colorBoxes.style.height = '16px';
-
   container.append(colorBoxes);
 }
 
 container.style.width = ` ${+gridSizes * 16}px`;
 container.style.height = ` ${+gridSizes * 16}px`;
+
+const colorBoxesAll = document.querySelectorAll('.color-box');
