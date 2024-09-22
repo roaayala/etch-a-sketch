9efd1eb05a1056ceaf9be197214d01a8a148ce5d
roaayala <roaayala.ahm@gmail.com>
2024-09-22 13:32:09 +0800

    set colorBoxes size relatives of parent

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const container = document.querySelector('.container');
+container.style.width = `80vw`;
+container.style.margin = '0 auto';
 
-// const gridSizes = prompt('How many pixel-grid do you want!');
+const gridSizes = prompt('How many grid do you want!');
 
 const randomizeColor = () => Math.floor(Math.random() * 255 + 1);
 
-// for (let i = 0; i < gridSizes ** 2; i++) {
-//   const colorBoxes = document.createElement('div');
-//   colorBoxes.style.backgroundColor = `rgb(${randomizeColor()}, ${randomizeColor()},${randomizeColor()})`;
-//   colorBoxes.setAttribute('class', 'color-box');
-//   colorBoxes.style.width = '16px';
-//   colorBoxes.style.height = '16px';
-//   container.append(colorBoxes);
-// }
-
-container.style.width = `100vw`;
-container.style.height = `100vh`;
+for (let i = 0; i < gridSizes ** 2; i++) {
+  const colorBoxes = document.createElement('div');
+  colorBoxes.style.backgroundColor = `rgb(${randomizeColor()}, ${randomizeColor()},${randomizeColor()})`;
+  colorBoxes.setAttribute('class', 'color-box');
+  colorBoxes.style.width = `calc(${container.style.width}
+     / ${+gridSizes}
+  )`;
+  colorBoxes.style.height = colorBoxes.style.width;
+  container.append(colorBoxes);
+}
 
 const colorBoxesAll = document.querySelectorAll('.color-box');
