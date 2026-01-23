2c3bdd4eb16e840f89daad0ca41bcb037a4844ca
roaayala <roaayala.ahm@gmail.com>
2026-01-23 21:03:53 +0800

    hover effect work

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 const container = document.querySelector('.container');
+
+const size = 16;
+
+for (let i = 1; i <= size * size; i++) {
+	const box = document.createElement('div');
+	box.setAttribute(
+		'style',
+		`background-color: black; width: ${600 / size}px; height: ${600 / size}px;`,
+	);
+
+	box.addEventListener('mouseenter', (event) => {
+		event.target.style.backgroundColor = `white`;
+	});
+
+	container.appendChild(box);
+}
