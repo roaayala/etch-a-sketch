2ecac8409b1b68efcbd7b20f5108233343f35d8c
roaayala <roaayala.ahm@gmail.com>
2026-01-24 21:28:06 +0800

    randomize color, but the color retain  same for each hovering

--- main.js
+++ main.js
@@ -4,17 +4,19 @@ function generateRandomColor() {
 	return Math.floor(Math.random() * 255);
 }
 
-const size = 32;
+const size = 16;
 
 for (let i = 1; i <= size * size; i++) {
 	const box = document.createElement('div');
 	box.setAttribute(
 		'style',
-		`background-color: grey; width: ${600 / size}px; height: ${600 / size}px;`,
+		`width: ${600 / size}px; height: ${600 / size}px; border: 1px solid black;`,
 	);
 
+	const color = `rgb(${generateRandomColor()},${generateRandomColor()},${generateRandomColor()})`;
+
 	box.addEventListener('mouseenter', (event) => {
-		event.target.style.backgroundColor = `rgb(${generateRandomColor()},${generateRandomColor()},${generateRandomColor()})`;
+		event.target.style.backgroundColor = color;
 	});
 
 	container.appendChild(box);
