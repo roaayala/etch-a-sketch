04829928f2b019c000febb630a5a51a24f8b718f
roaayala <roaayala.ahm@gmail.com>
2026-01-24 21:19:36 +0800

    randomize color on hover

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 const container = document.querySelector('.container');
 
-const size = 16;
+function generateRandomColor() {
+	return Math.floor(Math.random() * 255);
+}
+
+const size = 32;
 
 for (let i = 1; i <= size * size; i++) {
 	const box = document.createElement('div');
 	box.setAttribute(
 		'style',
-		`background-color: black; width: ${600 / size}px; height: ${600 / size}px;`,
+		`background-color: grey; width: ${600 / size}px; height: ${600 / size}px;`,
 	);
 
 	box.addEventListener('mouseenter', (event) => {
-		event.target.style.backgroundColor = `white`;
+		event.target.style.backgroundColor = `rgb(${generateRandomColor()},${generateRandomColor()},${generateRandomColor()})`;
 	});
 
 	container.appendChild(box);
